e96e18247bd6d49a46ab4efdce5194d3f19b92b3
인권 백 <titanbaek91@gmail.com>
2023-04-14 20:41:55 +0900

    feat : Question1 함수 구현
    문자열과 키워드(단어)를 입력 받아 문자열에서 입력 받은 키워드의 위치 값을 출력해주는 함수 구현

--- Today_BaeksGithub/LevelTest/Question.cs
+++ Today_BaeksGithub/LevelTest/Question.cs
@@ -8,5 +8,22 @@ namespace Today_BaeksGithub.LevelTest
 {
     internal class Question
     {
+        public static void Question1()
+        {
+            Console.Write("문자열을 입력하세요 :");
+            string dataStr = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write($"\"{dataStr}\" 에서 검색 할 단어를 입력하세요 :");
+            string searchStr = Console.ReadLine();
+
+            // 대,소문자에 따라 검색결과가 달라지기 때문에, 문자열과 검색어를 소문자로 만들어 주고 IndexOf를 진행
+            dataStr = dataStr.ToLower();
+            searchStr = searchStr.ToLower();
+
+            Console.WriteLine(dataStr.IndexOf(searchStr));
+
+        }
+
+
     }
 }
