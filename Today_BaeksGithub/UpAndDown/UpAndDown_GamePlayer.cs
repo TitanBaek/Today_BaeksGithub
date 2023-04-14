b1232c1ebbcbe9de3bb5ed48be86b9f465eb8ffc
인권 백 <titanbaek91@gmail.com>
2023-04-14 21:11:39 +0900

    feat : GamePlayer 변수 및 생성자 추가
    playername Getter&Setter 및 생성자 추가

--- Today_BaeksGithub/UpAndDown/UpAndDown_GamePlayer.cs
+++ Today_BaeksGithub/UpAndDown/UpAndDown_GamePlayer.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Today_BaeksGithub.UpAndDown
 {
-    internal class UpAndDown_GamePlayer
+    public class UpAndDown_GamePlayer
     {
+        private string playerName;
+        public string PLAYERNAME
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+
+
+        public UpAndDown_GamePlayer(string name)
+        {
+            this.playerName = name;
+        }
+
     }
 }
