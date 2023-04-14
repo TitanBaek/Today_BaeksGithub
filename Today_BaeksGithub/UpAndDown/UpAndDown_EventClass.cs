c09e0b44befb62f833c99eaac5fad2ee2a5abbb0
인권 백 <titanbaek91@gmail.com>
2023-04-14 21:05:29 +0900

    feat : UpAndDown EventClass 생성
    UpAndDown 구현을 위한 EventClass 생성

---
+++ Today_BaeksGithub/UpAndDown/UpAndDown_EventClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Today_BaeksGithub.UpAndDown
+{
+    internal class UpAndDown_EventClass
+    {
+    }
+}
