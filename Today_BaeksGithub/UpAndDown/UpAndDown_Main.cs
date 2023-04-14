80846129b6f19d44fc4a718cfd3f0b71ce42a7a9
인권 백 <titanbaek91@gmail.com>
2023-04-14 21:13:05 +0900

    feat : MainMenu 함수 추가
    게임 초기화면인 MainMenu 함수 추가

--- Today_BaeksGithub/UpAndDown/UpAndDown_Main.cs
+++ Today_BaeksGithub/UpAndDown/UpAndDown_Main.cs
@@ -8,5 +8,15 @@ namespace Today_BaeksGithub.UpAndDown
 {
     internal class UpAndDown_Main
     {
+        public static void MainMenu()
+        {
+            Console.Write("업&다운! 게임을 시작하겠습니까? (Y/N) :");
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "Y": PlayerSetting(); break;
+                case "N": return; break;
+                default: break;
+            }
+        }
     }
 }
